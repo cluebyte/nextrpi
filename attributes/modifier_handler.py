4742fdf5c28fe66f90de7f8dc3471586accd73da
Faye Yao <fyao610@gmail.com>
2016-07-15 23:51:29 -0700

    Moved resolving order of operations for modifiers into handler

--- attributes/modifier_handler.py
+++ attributes/modifier_handler.py
@@ -90,7 +90,27 @@ class ModifierHandler(object):
         Returns: Number
         """
         mods = self.all()
-        res = modifier_utils.resolve_modified_val(base_val, *mods)
+        res = self._resolve_modified_val(base_val)
+        return res
+
+    def _resolve_modified_val(base_val):
+        """Resolve order of operations for modifier parameters.
+
+        Return the modified value based on order of operations of the modifiers.
+
+        Arguments:
+        base_val (number) - base value we are modifying
+
+        Returns: Number
+        """
+        modifiers = self.all()
+        mul_div_ops = [mod for mod in modifiers if mod.operator in ["*","/"] ]
+        add_sub_ops = [mod for mod in modifiers if mod.operator in ["+", "-"] ]
+        res = base_val
+        for mod in mul_div_ops:
+            res = mod.get_modified_val(res)
+        for mod in add_sub_ops:
+            res = mod.get_modified_val(res)
         return res
 
     def serialize_all_mods(self):
@@ -102,8 +122,7 @@ class ModifierHandler(object):
         """
         mods = self.all()
         return [
-                mod.serialize() for mod in mods
-        ]
+                mod.serialize() for mod in mods]
 
     def __len__(self):
         return len(self._raw_modifiers)
--- attributes/modifier_utils.py
+++
@@ -1,9 +0,0 @@
-def resolve_modified_val(base_val, *modifiers):
-    mul_div_ops = [mod for mod in modifiers if mod.op in ["*","/"] ]
-    add_sub_ops = [mod for mod in modifiers if mod.op in ["+", "-"] ]
-    res = base_val
-    for mod in mul_div_ops:
-        res = mod.get_modified_val(res)
-    for mod in add_sub_ops:
-        res = mod.get_modified_val(res)
-    return res
