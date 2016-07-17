a7f9c4f25e7bd8d6060e7892375376b3778bc900
Faye Yao <fyao610@gmail.com>
2016-07-16 23:42:25 -0700

    added docstrings

--- attributes/observer_constants.py
+++ attributes/observer_constants.py
@@ -1,4 +1,11 @@
+"""
+Constants for AttributeObservers.
+"""
+
 class NotifyType(object):
+    """
+    Enum class for notification types.
+    """
     UPDATE = 1
     CREATE = 2
     DELETE = 3
--- attributes/resource_constants.py
+++ attributes/resource_constants.py
@@ -1,3 +1,10 @@
+"""
+Constants for resources.
+"""
+
 class AttributeType(object):
+    """
+    Enum class for resource attribute types, either MIN or MAX.
+    """
     MIN = 1
     MAX = 2
